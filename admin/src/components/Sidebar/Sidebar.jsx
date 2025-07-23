7f333269fb9be13658d3632a23e50b63ee79960f
anchal2539 <anamsaini83073@gmail.com>
2025-07-23 14:15:24 +0530

    added admin frontend

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <div className="sidebar-title"></div>
+      <ul className="sidebar-list">
+        <li><a href="/Dashboard">Dashboard</a></li>
+        <li><a href="/ManageAddUsers">ManageAddUsers</a></li>
+        <li><a href="/ApproveProjects">ApproveProjects</a></li>
+        <li><a href="/manageProject">manageProject</a></li>
+        <li><a href="/ReviewContributions">ReviewContributions</a></li>
+        <li><a href="/MonitorPayments">MonitorPayments</a></li>
+      </ul>
+    </div>
   )
 }
 
